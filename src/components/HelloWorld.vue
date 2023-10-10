7e16c870e0ec77a09fb84b68f1573c18d7957d53
Marioditrani <marioditrani2003@gmail.com>
2023-10-10 11:08:02 +0200

    fc

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,25 +2,20 @@
 export default{
   data() {
     return{
+      
       slides: [
-        {
-          id: 1,
-          image: "https://i0.wp.com/www.giacomocusano.com/wp-content/uploads/2016/07/coastal-wash-web.jpg?fit=1024%2C682&ssl=1"
-        },
-        {
-          id: 2,
-          image: "https://www.artemedialab.it/wp-content/uploads/2019/04/immagini-sfondo-1-700x400.jpg"
-        },
-        {
-          id: 3,
-          image: "https://fotografiaartistica.it/wp-content/uploads/2019/06/nasa-immagini-gratuite-dello-spazio.jpg"
-        },
-      ],
+        "https://i0.wp.com/www.giacomocusano.com/wp-content/uploads/2016/07/coastal-wash-web.jpg?fit=1024%2C682&ssl=1",
+        "https://www.artemedialab.it/wp-content/uploads/2019/04/immagini-sfondo-1-700x400.jpg",
+        "https://fotografiaartistica.it/wp-content/uploads/2019/06/nasa-immagini-gratuite-dello-spazio.jpg"],
+      
+      
+      
     };
+    
   },
   methods:{
-    imageRight(){
-      
+    imagedestra(){
+      this.slides[0]
     }
   }
 };
@@ -29,13 +24,10 @@ export default{
 <template>
   <div class="arrows">
       <div class="arrows-left" >&#129044;</div>
-      <div class="arrows-right">&#129046;</div>
+      <div class="arrows-right" @click="imagedestra">&#129046;</div>
     </div>
  <div class="carousel">
-    <div class="slide" v-for="slide in slides" :key="slide.id">
-      <img :src="slide.image">
-    </div>
-    
+      <img  v-for="slide in slides" :key="slide" :src="slides[0]">
  </div>
 </template>
 
@@ -43,6 +35,7 @@ export default{
 .carousel{
   display: flex;
   align-content: center;
+  
 }
 .arrows{
   display: flex;
